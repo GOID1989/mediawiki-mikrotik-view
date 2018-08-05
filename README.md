b79dadf93cd85068c244600d75eca313edbafad5
GOID1989 <only.snickers.work@gmail.com>
2018-08-05 21:51:08 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mediawiki-mikrotik-view
+
+Mediawiki Tag Extension for import NAT rules from Mikrotik.
+
+## Prerequisites
+ - PHP API for Mikrotik API https://github.com/BenMenking/routeros-api
+ - Enable Mikrotik API Services (IP->Services) and allow access to ports 8728-8729
+ - Put extension php-file to "extensions\mikrotikView" folder of Mediawiki
+ - Add line `require_once( "$IP/extensions/mikrotikView/mikrotikView.php" );` into MW's LocalSettings.php
+ 
+## How to use
+On Wiki article page in edit mode add `<mikrotik ip="192.168.88.1" login="admin" password="tooooooo_long_pwd"/>` (three attributes REQUIRED!). Save changes.
---
+++ mikrotikView.php
@@ -0,0 +1,72 @@
+<?php
+require_once( 'routeros_api.class.php' );
+
+$wgHooks['ParserFirstCallInit'][] = 'mikrotikViewSetup';
+
+function mikrotikViewSetup( Parser $parser ) {
+	$parser->setHook( 'mikrotik', 'mikrotikViewRender' );
+	return true;
+}
+
+function mikrotikViewRender( $input, array $args, Parser $parser, PPFrame $frame ) {
+	$parser->disableCache();
+	$API = new RouterosAPI();
+	#$API->debug = true;
+	
+	if(!isset($args['ip'])) { return "IP not set"; }
+	if(!isset($args['login'])) { return "Login not set"; }
+	if(!isset($args['password'])) { return "Password not set"; }
+	
+	if ($API->connect($args['ip'], $args['login'], $args['password'])) {
+		$API->write("/ip/firewall/nat/print");
+		$READ = $API->read(false);
+		$ARRAY = $API->parseResponse($READ);
+		
+		$tbl = "<table class='wikitable'>";
+		$tbl = $tbl."<th>Внешний порт</th>
+				<th>Внутренний порт</th>
+				<th>Внутренний адрес</th>
+				<th>Ограничение доступа</th>
+				<th>Комментарий</th>";
+		foreach( $ARRAY as $arr) {
+			if($arr["action"] != "masquerade" and $arr["action"] != "redirect" ) {
+				$tbl = $tbl."<tr><td>".(array_key_exists("dst-port", $arr)? $arr["dst-port"]: "")."</td>"; 
+				$tbl = $tbl."<td>".(array_key_exists("to-ports", $arr)? $arr["to-ports"]: "")."</td>"; 
+				$tbl = $tbl."<td>".(array_key_exists("to-addresses", $arr)? $arr["to-addresses"]: "")."</td>"; 
+
+				# Combine two field with source IP into one cell
+				$sourceIPs = "";
+				$sourceIPs .= array_key_exists("src-address-list", $arr) ? $arr["src-address-list"].", " : "" ;
+				$sourceIPs .= array_key_exists("src-address", $arr) ? $arr["src-address"].", " : "" ;
+				$tbl = $tbl."<td>".$sourceIPs."</td>";
+				
+				$tbl = $tbl."<td>".(array_key_exists("comment", $arr)? $arr["comment"]: "")."</td>"; 
+				# Close table row
+				$tbl = $tbl."</tr>";
+			}
+		}
+		
+		$API->write("/ip/firewall/address-list/print");
+		$READ = $API->read(false);
+		$ARRAY = $API->parseResponse($READ);
+		$tbl = $tbl."<tr><td colspan='5'>Списки доступа</td></tr>";
+		$ADDRESS_LISTS = array();
+		foreach( $ARRAY as $arr) {
+			if(array_key_exists($arr["list"], $ADDRESS_LISTS)) {
+				$ADDRESS_LISTS[$arr["list"]] = $ADDRESS_LISTS[$arr["list"]]. ", ".$arr["address"];
+			}
+			else {
+				$ADDRESS_LISTS[$arr["list"]] = $arr["address"];
+			}
+		}
+		foreach($ADDRESS_LISTS as $key => $value){
+			$tbl = $tbl."<tr><td>".$key."</td><td colspan='4'>".$value."</td></tr>";
+		}
+
+		$API->disconnect();
+		return $tbl."</table>"."\n\n" . htmlspecialchars( $input );
+	}
+	else { return "Ошибка подключения"; }
+}
+
+?>
